3280112049d2e8a1de53773ddfa76974b6b0eccc
TaiVT <>
2024-02-10 21:05:10 +0700

    Init project

---
+++ src/main/java/com/github/taivtse/sieve/Main.java
@@ -0,0 +1,11 @@
+package com.github.taivtse.sieve;
+
+/**
+ * @author taivt
+ * @since $YEAR/$MONTH/$DAY $HOUR:$MINUTE:$SECOND
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
